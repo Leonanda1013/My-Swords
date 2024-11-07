c5aeb86c9f80d67ca29032a55b68367a97b5a15a
leonanda1013 <leonandaprabowo@gmail.com>
2024-11-07 11:35:08 +0700

    baru buka lagi

---
+++ Python/oprasiAritmatika.py
@@ -0,0 +1,16 @@
+a = 10
+b = 3
+
+hasil = a+b
+print(a,'+',b ,'=', hasil)
+
+a = True
+b = False
+hasil = a or b
+print(hasil)
+hasil = a and b 
+print(hasil)
+hasil = a ^ b
+print(hasil)
+hasil = a^a
+print(hasil)
