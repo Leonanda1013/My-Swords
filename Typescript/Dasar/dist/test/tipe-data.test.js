426fc95422561814ab391e8ef7eb7e6367af677f
Vincentius <leonandaprabowo@gmail.com>
2026-02-15 23:41:10 +0700

    Typescript: Tipe data primitif

---
+++ Typescript/Dasar/dist/test/tipe-data.test.js
@@ -0,0 +1,12 @@
+describe('Data Type', function () {
+    it('should mush declare', function () {
+        const name = "Vincentius Leonanda Prabowo";
+        const balance = 1000000;
+        const isRich = true;
+        console.info(name);
+        console.info(balance);
+        console.info(isRich);
+    });
+});
+export {};
+//# sourceMappingURL=tipe-data.test.js.map
---
+++ Typescript/Dasar/test/tipe-data.test.ts
@@ -0,0 +1,12 @@
+describe('Data Type', function(){
+    it('should mush declare', function(){
+
+        const name : String = "Vincentius Leonanda Prabowo"
+        const balance : Number = 1000000
+        const isRich : Boolean = true
+
+        console.info(name);
+        console.info(balance);
+        console.info(isRich);
+    })
+})
