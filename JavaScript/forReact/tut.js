c6eea8f29e6ea1d0712392aea6552a7925c2db5d
Vincentius <leonandaprabowo@gmail.com>
2026-02-17 21:39:25 +0700

    Javascript: Map Filter

--- JavaScript/forReact/tut.js
+++ JavaScript/forReact/tut.js
@@ -8,4 +8,14 @@ let suka = mood === "happy" ? "I am happy" : "I am sad";
 
 //  map filter
 
+// map digunakan untuk mengubah setiap elemen dalam array menjadi bentuk yang baru
 
+const numbers = [1, 2, 3, 4, 5];
+
+const squaredNumbers = numbers.map((number) => number * number);
+
+// filter digunakan untuk menyaring elemen dalam array berdasarkan kondisi tertentu
+const evenNumbers = numbers.filter((number) => number % 2 === 0);
+
+// reduce digunakan untuk mengakumulasi nilai dari elemen dalam array menjadi satu nilai tunggal
+const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
