d4cad6484a81f18c9973008d60abd0e4bf6c0d70
Vincentius <leonandaprabowo@gmail.com>
2026-08-03 19:37:50 +0700

    C++ oop : class, object, method, constuctor

---
+++ c++/oop.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+class Employee{
+public:    
+    string Name;
+    string Company;
+    int Age;
+
+    void IntroduceYourself(){
+        cout << "Name - " << Name << " From - " << Company << " Age - " << Age << endl;
+    }
+
+    Employee(string name, string company, int age){ //this is a constructor, it is a special method that is called when an object of the class is created
+        Name = name;
+        Company = company;
+        Age = age;
+    }
+};
+
+int main(){
+    int number; //this is a variable of type int
+    number = 10; //assigning a value to the variable
+
+    Employee employee1("John", "Youtube", 22); //this is an object of the class Employee 
+    // employee1.Name = "John"; //assigning a value to the Name attribute of employee1 
+    // employee1.Company = "Youtube";
+    // employee1.Age = 22;  // this is without using constructor, we can also assign values to the attributes of the object using constructor as shown below
+    employee1.IntroduceYourself(); //calling the method IntroduceYourself of employee1 object
+}
