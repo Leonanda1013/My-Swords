f444ed5071637212f1c0308376e944c8574d5648
Vincentius <leonandaprabowo@gmail.com>
2026-08-12 19:07:14 +0700

    CP 7 Agustus : soal 3

---
+++ CompetitivePrograming/7Agustus/soal3.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    if(n & n-1){
+        cout << "Tidak" <<endl;
+    } else {
+        cout << "Ya" <<endl;
+    }
+}
